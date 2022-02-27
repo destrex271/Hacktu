dc43f11de90d61191e2f827e33b588868ac8973c
destrex271 <destrex271@gmail.com>
2022-02-27 10:28:02 +0530

    Final

--- Frontend/screens/home.js
+++ Frontend/screens/home.js
@@ -6,43 +6,80 @@ import BackSVG from '../assets/images/backg.svg'
 import { homeStyles } from "../styles/homeStyles";
 import LogoSvg from '../assets/images/myShare.svg';
 import User from '../assets/images/User.svg'
+import HomeS from '../assets/images/home.svg'
+import QR from '../assets/images/qr.svg'
+import St from '../assets/images/st.svg'
 
 export const Home = ({ navigation }) => {
-    const backToHome = () => {
-        navigation.goBack()
-    }
-    const arrow = "<"
     return(
-        <SafeAreaView style={[global.back]}>
-            
-            <View style={[homeStyles.cont]}>
-                <View style={[homeStyles.profView, homeStyles.bottomShadow]}>
-                    <View style={[homeStyles.profileView, homeStyles.bottomShadow]}>
-                        <User
-                        width="100"
-                        />
+        <View style={global.back}>
+            <View style={[homeStyles.flexer]}>
+                <View style={[homeStyles.round, homeStyles.bottomShadow]}>
+                    <View
+                    style={{
+                        flex: 0.2
+                    }}
+                    >
+                        <View style={{
+                            alignSelf:"flex-start",
+                            marginTop: 10,
+                            paddingTop: 20,
+                            paddingLeft: 20
+                        }}>
+                            <Text style={[homeStyles.title]}>User Name</Text>
+                        </View>
+                        <View style={{
+                            alignSelf:"flex-start",
+                            paddingLeft: 20
+                        }}>
+                            <Text style={[homeStyles.subT]}>+91 9999999999</Text>
+                        </View>
                     </View>
-                    <View style={homeStyles.cont1}>
-                        <View style={homeStyles.nameView}>
-                            <Text style={homeStyles.namet}>Name Surname</Text>
-                            <Text style={homeStyles.numb}>+919999999999</Text>
-                        </View>
-                        <View style={homeStyles.credView}>
-                            <Text style={homeStyles.namet2}>Total Pending Amt : </Text>
-                            <Text style={homeStyles.amt}>₹ 1000</Text>
-                        </View>
-                        <View style={homeStyles.credView2}>
-                            <Text style={homeStyles.namet2}>Total Recieved Amt : </Text>
-                            <Text style={homeStyles.amt}>₹ 1000</Text>
-                        </View>
+                    <View style={{
+                        alignSelf:"flex-start",
+                        paddingTop: 20,
+                        marginTop: 40,
+                        paddingLeft: 20,
+                        flex: 0.1,
+                        flexDirection: "row"
+                    }}>
+                        <Text style={[homeStyles.subT2]}>Total Pending Amount: </Text><Text style={[homeStyles.subT3]}>₹ 1000</Text>
+                    </View>
+                    <View style={{
+                        alignSelf:"flex-start",
+                        // paddingTop: 20,
+                        paddingLeft: 20,
+                        flex: 0.1,
+                        flexDirection: "row"
+                    }}>
+                        <Text style={[homeStyles.subT2]}>Total Pending Amount: </Text><Text style={[homeStyles.subT3]}>₹ 1000</Text>
                     </View>
                 </View>
             </View>
-            <View style={homeStyles.cont3}>
-                <Text style={homeStyles.pend}>Pending Transaction</Text>
-                <ScrollView style={homeStyles.round}></ScrollView>
+            <View style={[homeStyles.flexer, homeStyles.margT300]}>
+                <View style={[homeStyles.round2, homeStyles.bottomShadow]}>
+                    <Text style={homeStyles.title}>Pending Transactions</Text>
+                </View>
             </View>
-            
-        </SafeAreaView>
+            <View style={[homeStyles.flexer]}>
+                <View style={[homeStyles.round2, homeStyles.bottomShadow]}>
+                    <Text style={homeStyles.title}>Recent Transactions</Text>
+                </View>
+            </View>
+            <View style={[homeStyles.nav]}>
+                <HomeS
+                width="50"
+                height="50"
+                />
+                <QR
+                width="50"
+                height="50"
+                />
+                <St
+                width="50"
+                height="50"
+                />
+            </View>
+        </View>
     )
 }
--- Frontend/styles/homeStyles.js
+++ Frontend/styles/homeStyles.js
@@ -1,40 +1,33 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 
 export const homeStyles = StyleSheet.create({
-    containerTop:{
-        flex: 0.1,
-        flexDirection: "row",
-        marginTop: -10,
-        justifyContent: "center",
-        paddingTop: 25,
-        paddingLeft: 10,
+    flexer: {
+        flex: 0.3,
+        padding: 10,
+        alignContent:"center",
+        alignItems:"center",
+        justifyContent: "center"
     },
-    cont: {
-        flex: 0.5,
-        justifyContent: "center",
-        alignItems:"center"
-    },
-    profView:{
-        flex: 0.65,
-        width: 370,
-        height: 200,
-        marginTop: -30,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 40,
-        elevation: 8,
-        padding: 8,
-        backgroundColor: '#272727'
-    },
-    roundBtn:{
-        width: 60,
-        height: 60,
+    round:{
+        width: 400,
+        height: 400,
         justifyContent: 'center',
         alignItems: 'center',
         paddingTop: 3,
         paddingRight: 2,
-        borderRadius: 100,
-        elevation: 3,
+        borderRadius: 50,
+        elevation: 6,
+        backgroundColor: '#272727'
+    },
+    round2:{
+        width: Dimensions.get('screen').width,
+        height: 200,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 3,
+        paddingRight: 2,
+        borderRadius: 50,
+        elevation: 6,
         backgroundColor: '#272727'
     },
     bottomShadow: {
@@ -46,94 +39,33 @@ export const homeStyles = StyleSheet.create({
         shadowRadius: 6,
         shadowColor: "#b6c3db"
     },
-    profileView: {
-        marginTop:90,
-        width: 100,
-        height: 100,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingTop: 0,
-        paddingRight: 0,
-        borderRadius: 100,
-        elevation: 5,
-        backgroundColor: '#474747',
-        alignSelf: "flex-end"
+    margT300: {
+        marginTop: 100
     },
-    nameView: {
-        alignSelf: "flex-start",
-        marginTop: -75
-    },
-    namet: {
+    title: {
         fontFamily: "PoppinsBold",
-        fontSize: 20
+        fontSize: 23,
+        color: "#ffffff"
     },
-    numb: {
+    subT: {
         fontFamily: "PoppinsBold",
         fontSize: 15,
-        color: "#ffffff78"
+        color: "#ffffff73"
     },
-    cont1: {
-        alignSelf: "flex-start",
-        marginTop: -10
-    },
-    namet2: {
-        fontFamily: "PoppinsBold",
-        fontSize: 16
-    },
-    amt: {
+    subT2: {
         fontFamily: "PoppinsBold",
         fontSize: 16,
-        color: "#DF3030",
-        marginLeft: 10
+        color: "#ffffff"
     },
-    credView: {
-        flex: 1,
-        flexDirection: "row",
-        alignItems: "flex-start",
-        marginTop: 0,
-    },
-    credView2: {
-        flex: 1,
-        flexDirection: "row",
-        alignItems: "flex-start",
-        marginTop: -190,
-    },
-    cont3: {
-        marginTop: 10,
-        paddingLeft: 30
-    },
-    pend: {
+    subT3: {
         fontFamily: "PoppinsBold",
-        fontSize: 22
+        fontSize: 16,
+        color: "#DF3030"
     },
-    round:{
-        
-        width: 367,
-        height: 200,
-        paddingTop: 3,
-        paddingRight: 2,
-        borderRadius: 20,
-        elevation: 3,
-        backgroundColor: '#272727',
-        marginLeft: -16,
-    },
-    cont4: {
-        marginTop: 10,
-        paddingLeft: 30
-    },
-    pend: {
-        fontFamily: "PoppinsBold",
-        fontSize: 22
-    },
-    round:{
-        width: 367,
-        height: 200,
-        paddingTop: 3,
-        paddingRight: 2,
-        borderRadius: 20,
-        elevation: 3,
-        backgroundColor: '#272727',
-        marginLeft: -16,
-    },
-    
+    nav:{
+        paddingHorizontal: 20,
+        flex:0.1,
+        flexDirection:"row",
+        justifyContent: "space-between"
+    }
 })
